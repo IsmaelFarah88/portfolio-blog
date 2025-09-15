6e9743c60c46b3d1563834a323239d0a01170144
Ismael Farah <167617802+IsmaelFarah88@users.noreply.github.com>
2025-09-15 21:33:53 +0330

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // استخدام Webpack بدلاً من Turbopack
+  turbo: false,
+
+  // خيارات أخرى يمكن إضافتها حسب الحاجة
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
